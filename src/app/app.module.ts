15a4a14744bcb5c3d065e6473a9580d237adfd81
Mazlum <mazlumgcl@googlemail.com>
2022-01-06 19:44:06 +0100

    second commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InterfaceComponent } from './interface/interface.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InterfaceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
