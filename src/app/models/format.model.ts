728d61da5a1317fe561e0d9c35f23d1ae3706cfd
Mazlum <mazlumgcl@googlemail.com>
2022-01-06 22:59:54 +0100

    model created

---
+++ src/app/models/format.model.ts
@@ -0,0 +1,9 @@
+export class Format {
+    public firstAttribute: string;
+    public secondAttribute: string;
+
+    constructor(firstAttribute: string, secondAttribute: string) {
+        this.firstAttribute = firstAttribute;
+        this.secondAttribute = secondAttribute;
+    }
+}
